4d76c8c008f2381a1e9b6b9624e871065ec1bdf5
ArbaazBaig <arbaazbaig98@gmail.com>
2025-01-20 17:20:12 +0530

    basename router

--- src/App.js
+++ src/App.js
@@ -11,16 +11,16 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 
 const App =()=> {
 
-useEffect(() => {
-  alanBtn({
-    // key: 'f9e1602fba686635e3db1a533c4b22df2e956eca572e1d8b807a3e2338fdd0dc/stage',
-    onCommand: (commandData) => {
-      if (commandData.command === 'go:back') {
-        // Call the client code that will react to the received command
-      }
-    }
-  });
-}, []);
+// useEffect(() => {
+//   alanBtn({
+//     // key: 'f9e1602fba686635e3db1a533c4b22df2e956eca572e1d8b807a3e2338fdd0dc/stage',
+//     onCommand: (commandData) => {
+//       if (commandData.command === 'go:back') {
+//         // Call the client code that will react to the received command
+//       }
+//     }
+//   });
+// }, []);
   const pageSize=6;
   const apiKey=process.env.REACT_APP_NEWS_API_KEY
     return (
@@ -31,14 +31,14 @@ useEffect(() => {
       // btn={btn}
       // search={search}
     />
-      <Routes> 
-      <Route exact path="/" element={<News key="general" pageSize={pageSize} apiKey={apiKey}  country='in' category='general'/>} />
-      <Route exact path="/sports" element={<News key="sports"  pageSize={pageSize} apiKey={apiKey} country='in' category='sports'/>} />
-      <Route exact path="/business" element={<News key="business" pageSize={pageSize} apiKey={apiKey} country='in' category='business'/>} />
-      <Route exact path="/entertainment" element={<News key="entertainment" pageSize={pageSize} apiKey={apiKey} country='in' category='entertainment'/>} />
-      <Route exact path="/technology" element={<News key="technology" pageSize={pageSize} apiKey={apiKey} country='in' category='technology'/>} />
-      <Route exact path="/general" element={<News key="general" pageSize={pageSize}  apiKey={apiKey} country='in' category='general'/>} />
-      <Route exact path="/science" element={<News key="science" pageSize={pageSize} apiKey={apiKey} country='in' category='science'/>} />
+      <Routes basename='/RNes'> 
+      <Route exact path="/" element={<News key="general" pageSize={pageSize} apiKey={apiKey}  country='us' category='general'/>} />
+      <Route exact path="/sports" element={<News key="sports"  pageSize={pageSize} apiKey={apiKey} country='us' category='sports'/>} />
+      <Route exact path="/business" element={<News key="business" pageSize={pageSize} apiKey={apiKey} country='us' category='business'/>} />
+      <Route exact path="/entertainment" element={<News key="entertainment" pageSize={pageSize} apiKey={apiKey} country='us' category='entertainment'/>} />
+      <Route exact path="/technology" element={<News key="technology" pageSize={pageSize} apiKey={apiKey} country='us' category='technology'/>} />
+      <Route exact path="/general" element={<News key="general" pageSize={pageSize}  apiKey={apiKey} country='us' category='general'/>} />
+      <Route exact path="/science" element={<News key="science" pageSize={pageSize} apiKey={apiKey} country='us' category='science'/>} />
       </Routes>
     </Router>
       </div>
